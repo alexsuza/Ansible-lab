b9e6be071f6a9d3e655f322e2808d88648b3c0f7
alexsuza <alexdaysouza@gmail.com>
2023-08-10 20:27:55 -0600

    update install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,15 @@ sudo pip3 install ansible
 
 #install boto3 and botocore
 sudo pip3 install boto3
+
+#install git
+sudo yum install git -y
+
+#clone my code
+git clone https://github.com/alexsuza/ansible-lab.git
+
+#copy code
+cp -r ansible-lab/* .
+
+# remove the cloned directory
+rm -rf ansible-lab
